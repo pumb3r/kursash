eb2418dc2e29795c6a5ba851c149d2bfd70630e6
Миша <58284931+pumb3r@users.noreply.github.com>
2020-03-31 10:26:51 +0300

    добавив drag drop

---
+++ puzzle/puzzle/puzzle/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace puzzle
+{
+    partial class Puzzle
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Puzzle));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.picture1box = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьКартинкуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture1box)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(518, 111);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(270, 270);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // picture1box
+            // 
+            this.picture1box.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.picture1box.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picture1box.Location = new System.Drawing.Point(33, 111);
+            this.picture1box.Name = "picture1box";
+            this.picture1box.Size = new System.Drawing.Size(270, 270);
+            this.picture1box.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picture1box.TabIndex = 0;
+            this.picture1box.TabStop = false;
+            this.picture1box.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.picture1box.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picture1box_MouseDown);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.загрузитьКартинкуToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.файлToolStripMenuItem.Text = "   Файл";
+            // 
+            // загрузитьКартинкуToolStripMenuItem
+            // 
+            this.загрузитьКартинкуToolStripMenuItem.Name = "загрузитьКартинкуToolStripMenuItem";
+            this.загрузитьКартинкуToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.загрузитьКартинкуToolStripMenuItem.Text = "Загрузить картинку";
+            this.загрузитьКартинкуToolStripMenuItem.Click += new System.EventHandler(this.загрузитьКартинкуToolStripMenuItem_Click);
+            // 
+            // Puzzle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picture1box);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Puzzle";
+            this.Text = "Puzzle";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.Puzzle_Load);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Puzzle_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture1box)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox picture1box;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьКартинкуToolStripMenuItem;
+    }
+}
+
---
+++ puzzle/puzzle/puzzle/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace puzzle
+{
+    public partial class Puzzle : Form
+    {
+        public Puzzle()
+        {
+            InitializeComponent();
+            picture1box.MouseDown += picture1box_MouseDown;
+            pictureBox2.AllowDrop = true;
+            pictureBox2.DragEnter += pictureBox2_DragEnter;
+            pictureBox2.DragDrop += pictureBox2_DragDrop;
+        //https://stackoverflow.com/questions/16004682/c-sharp-drag-and-drop-from-one-picture-box-into-another
+        }
+           
+
+        
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+
+        private void Puzzle_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void picture1box_MouseDown(object sender, MouseEventArgs e)
+        {
+            var img = picture1box.Image;
+            if (img == null) return;
+            if (DoDragDrop(img, DragDropEffects.Move) == DragDropEffects.Move)
+            {
+                picture1box.Image = null;
+            }
+        }
+
+        private void Puzzle_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Bitmap))
+                e.Effect = DragDropEffects.Move;
+        }
+
+        private void pictureBox2_DragDrop(object sender, DragEventArgs e)
+        {
+            var bmp = (Bitmap)e.Data.GetData(DataFormats.Bitmap);
+            pictureBox2.Image = bmp;
+        }
+
+        private void загрузитьКартинкуToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog oFd = new OpenFileDialog();
+            // формат картинки
+            oFd.Filter = "Image Files(*.BMP;*.JPG;*.PNG;*.)|*.BMP;*.JPG;*.PNG|All files(*.*)|*.*";//Image Files(*.BMP;*.JPG;*.PNG;*.) вибор формата файла ,*.BMP;*.JPG;*.PNG це файли які нам підходять і можна вибрати
+                                                                                                  // *- це назва файлу 
+
+
+            if (oFd.ShowDialog() == DialogResult.OK)// тут наступний крок якщо нажали ок и вибрали картинку 
+            {
+                try// ми загружаэм картинку в пикчер бокс
+                {
+                    picture1box.Image = new Bitmap(oFd.FileName);
+                }
+                catch// якщо у нас буде якась помилка
+                {
+                    MessageBox.Show("Не возможно открить файл", "ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+        }
+    }
+}
